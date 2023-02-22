9fa777d7b81e4e966ad68aa514d33d289d7d8389
kaixuan <kaixuant@yahoo.com>
2023-02-22 09:20:20 +0800

    initialise posts component, get api json
    call get api in constructor

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css'],
+})
+export class PostsComponent {
+  posts: any[] = [];
+
+  constructor(http: HttpClient) {
+    http
+      .get('http://jsonplaceholder.typicode.com/posts')
+      .subscribe((response) => {
+        this.posts = response as any[];
+      });
+  }
+}
